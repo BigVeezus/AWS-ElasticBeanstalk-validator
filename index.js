318cc01ea24292e20c2d5c419d86a3e99607dd67
Elvis O <elvis.osujic@gmail.com>
2023-05-25 23:23:23 +0100

    Added password regex

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ app.post("/register", async function (req, res, next) {
     });
   }
 
+  var pattern = new RegExp(
+    /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*?()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*?()_+]{5,19}$/
+  );
+  if (!pattern.test(password)) {
+    return res.status(400).send({
+      message: "Password must contain 1 number and 1 special character",
+    });
+  }
+
   if (number[0] != 0) {
     return res.status(400).send({
       message: "Phone number must start with zero",
